72d1a9693f124cfdc6cc679afab710971fe108b1
MRizkiAG <mrizkiag1027@gmail.com>
2021-12-07 21:17:22 +0800

    First-commit
    Latihan implementasi PHP MVC

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/wpu/WPU-MVC/public');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
